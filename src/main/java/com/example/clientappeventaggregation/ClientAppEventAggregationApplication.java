e18533bed66e79ba92db8dd855b9a0bb5b43f15f
Adriane0291 <adrianbloch01@gmail.com>
2023-01-06 12:25:01 +0100

    -added  basic configuration (important, server port = 8083)

---
+++ src/main/java/com/example/clientappeventaggregation/ClientAppEventAggregationApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientappeventaggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAppEventAggregationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientAppEventAggregationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clientappeventaggregation/ClientAppEventAggregationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientappeventaggregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAppEventAggregationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
